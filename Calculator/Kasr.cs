9e58d6319c8a4d840ea99d283aaeb6285ba996c8
Haydarovcoder <abboshaydarov0799@gmail.com>
2022-11-25 18:14:55 +0500

    first commit

---
+++ Calculator/Kasr.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Kasr
+    {
+
+        #region kasr sonning surat va maxraji uchun o'zgaruvchilar yaratdik
+        public long  surat;
+        public long maxraj;
+        #endregion
+
+        #region kasr sonlar uchunn propertylar yaratdik
+        public long Surat
+        {
+            get
+            {
+                return surat;
+            } 
+                }
+        public long Maxraj 
+        {
+            get
+            {
+                return maxraj;
+            }
+        }
+        #endregion
+
+        public Kasr(long surat, long maxraj)
+        {
+            this.surat = surat;
+            this.maxraj = maxraj;
+        }
+
+        public override string ToString()
+        {
+            return surat + "/" + maxraj;
+        }
+
+        #region EKUB va EKUK ni topish
+        public static long EKUB(long a, long b)
+        {
+            long Remainder;
+
+            while (b != 0)
+            {
+                Remainder = a % b;
+                a = b;
+                b = Remainder;
+            }
+
+            return a;
+        }
+
+        public static long EKUK(long a, long b)
+        {
+            long num1, num2;
+            if (a > b)
+            {
+                num1 = a; num2 = b;
+            }
+            else
+            {
+                num1 = b; num2 = a;
+            }
+
+            for (int i = 1; i <= num2; i++)
+            {
+                if ((num1 * i) % num2 == 0)
+                {
+                    return i * num1;
+                }
+            }
+            return num2;
+        }
+        #endregion
+
+
+        public static long KasrQisqartirish(long surat, long maxraj)
+        {
+            if (surat == maxraj)
+                return 1;
+            else
+                return EKUB(surat, maxraj);
+        }
+
+        public static long UmumiyMaxraj(Kasr son1, Kasr son2)
+        {
+            long result = 0;
+
+            if (son1.maxraj == son2.maxraj)
+                return son1.maxraj;
+
+            else if (son1.maxraj != son2.maxraj)
+            {
+                son1.surat = son1.surat * (EKUK(son1.maxraj, son2.maxraj) / son1.maxraj);
+                son2.surat = son2.surat * (EKUK(son1.maxraj, son2.maxraj) / son2.maxraj);
+                // to do
+                return EKUK(son1.maxraj, son2.maxraj);
+            }
+
+            else if (son1.maxraj == 0)
+                return 0;
+
+            else if (son2.maxraj == 0)
+                return 0;
+
+            else // yo'lini topsak shu qatordi yuqotamiz
+                return result;
+ 
+        }
+                    
+        public static long Plus(Kasr son1, Kasr son2)
+        {
+           
+            UmumiyMaxraj(son1, son2);
+            son1.surat =  son1.surat + son2.surat;
+
+            return KasrQisqartirish(son1.surat, UmumiyMaxraj(son1, son2));
+        }
+
+        public static long Minus(Kasr son1, Kasr son2)
+        { 
+            UmumiyMaxraj(son1, son2);
+            son1.surat = son1.surat - son2.surat;
+
+            return KasrQisqartirish(son1.surat, UmumiyMaxraj(son1, son2));
+        }
+
+        public static long Multiply(Kasr son1, Kasr son2)
+        {
+            son1.surat =  son1.surat * son2.surat;
+            son1.maxraj = son1.maxraj * son2.maxraj;
+
+            return KasrQisqartirish(son1.surat, son1.maxraj);
+        }
+
+        public static long Divide(Kasr son1, Kasr son2)
+        {
+            son1.surat = son1.surat * son2.maxraj;
+            son1.maxraj = son2.surat * son1.maxraj;
+
+            return KasrQisqartirish(son1.surat, son1.maxraj);
+        }
+
+
+    }
+}
---
+++ Calculator/Menu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Menu
+    {
+       public void ShowMenuu()
+        {
+            string message = "\n";
+
+            message += "[1] - Plus\n\n";
+            message += "[2] - Minus\n\n";
+            message += "[3] - Multiply\n\n";
+            message += "[4] - Divide\n\n";
+            message += "[5] - Close\n\n";
+
+            Console.WriteLine("# # # # # # KASR SONLAR USTIDA AMAL BAJARADIGAN KALKULYATOR # # # # # #");
+
+            Console.WriteLine(message);
+            Console.Write("Tanlovingiz: ");
+
+            int.TryParse(Console.ReadLine(), out int tanlov);
+            switch (tanlov)
+            {
+                case 1:
+                    PlusUchun();
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+                
+            }
+
+        }
+
+        public void PlusUchun()
+        {
+            Console.WriteLine("Birinchi sonning suratini kiriting: ");
+
+            long surat;
+            long.TryParse(Console.ReadLine(), out surat);
+
+            Console.WriteLine("Birinchi sonning maxraj kiriting : ");
+
+            long maxraj;
+            long.TryParse(Console.ReadLine(), out maxraj);
+
+            Kasr son1 = new Kasr(surat, maxraj);
+
+            Console.WriteLine("Ikkinchi sonning suratini kiriting: ");
+
+            long surat2;
+            long.TryParse(Console.ReadLine(), out surat2);
+
+            Console.WriteLine("Ikkinchi sonning maxraj kiriting : ");
+
+            long maxraj2;
+            long.TryParse(Console.ReadLine(), out maxraj2);
+
+            Kasr son2 = new Kasr(surat2, maxraj2);
+
+            
+            // Console.WriteLine($"{surat}/{maxraj} + {surat2}/{maxraj2} = " + Kasr.Plus());
+            Console.WriteLine($"test: {son1} + {son2} = {Kasr.Plus(son1, son2)}");
+            //Kasr.Plus(son1, son2);
+        }
+
+    }
+}
